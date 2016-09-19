55471d1c138adfc10d6ef0c24614bb3dd84ff06b
Phan Minh <phanminh65@gmail.com>
2016-09-20 00:09:46 +0700

    update api, DATABASE UPDATE

--- backend-4c/Api/Controllers/Login.php
+++ backend-4c/Api/Controllers/Login.php
@@ -7,19 +7,36 @@
  */
 
 namespace Api\Controllers;
+
 use Api\Controllers\ApiController as MainController;
 
 class Login extends MainController
 {
-    public function indexAction() {
-//        $this->responseApi(0, 'test', $_GET);
+    public function indexAction()
+    {
         global $connection;
         $co = $connection->getCo();
-        if (!empty($_COOKIE['user']) && !empty($_COOKIE['token']) && !empty($_SESSION['User'])) {
-                $this->responseApi(0, 'You already loggedin!', $_SESSION['User']);
-        }
-        if (!empty($_GET['username']) && !empty($_GET['password'])) {
-
+        if (!empty($_COOKIE['user']) && !empty($_COOKIE['token']) || !empty($_SESSION['User'])) {
+            $this->responseApi(0, 'You already loggedin!', $_SESSION['User']);
+        } elseif (!empty($_GET['login']) && !empty($_GET['password'])) {
+            $token = md5(uniqid() . time());
+            $modelUser = new \Administration\Models\User($co);
+            $result = $modelUser->getUserLogin($_GET['login'], $_GET['password']);
+            if (!empty($result)) {
+                $_SESSION['User']['id'] = $result->id;
+                $_SESSION['User']['username'] = $result->username;
+                $_SESSION['User']['id_role'] = $result->id_role;
+                $_SESSION['User']['token'] = $token;
+                $modelUser->updateToken($token, $result->id);
+                setcookie("user", $_GET['login'], time() + (86400 * 30));
+                setcookie("token", "$token", time() + (86400 * 30));
+                $modelUser->updateLastLogin(date("Y:m:d H:i:s"), $result->id);
+                $this->responseApi(0, 'Login successfully', $_SESSION['User']);
+            } else {
+                $this->responseApi(110001);
+            }
+        } else {
+            $this->responseApi(100003);
         }
     }
 }
--- backend-4c/at_mobile.sql
+++ backend-4c/at_mobile.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Host: 127.0.0.1
--- Generation Time: Sep 14, 2016 at 07:03 PM
+-- Generation Time: Sep 19, 2016 at 07:08 PM
 -- Server version: 10.1.16-MariaDB
 -- PHP Version: 5.6.24
 
@@ -19,6 +19,23 @@ SET time_zone = "+00:00";
 --
 -- Database: `at_mobile`
 --
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `bill`
+--
+
+CREATE TABLE `bill` (
+  `id` int(11) UNSIGNED NOT NULL,
+  `profile_id` int(11) UNSIGNED NOT NULL,
+  `user_id` int(11) UNSIGNED NOT NULL,
+  `order_id` int(11) UNSIGNED NOT NULL,
+  `per_cash` int(20) NOT NULL,
+  `total_cash` int(20) NOT NULL,
+  `created` datetime NOT NULL,
+  `updated` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 -- --------------------------------------------------------
 
@@ -102,6 +119,7 @@ CREATE TABLE `product` (
   `title` varchar(255) NOT NULL COMMENT 'tên sp',
   `params` varchar(255) NOT NULL COMMENT 'link param trên url',
   `price` int(20) UNSIGNED NOT NULL,
+  `count` int(11) NOT NULL,
   `active` tinyint(1) NOT NULL,
   `detail` text NOT NULL COMMENT 'thông tin',
   `type` varchar(255) NOT NULL,
@@ -187,12 +205,37 @@ INSERT INTO `user` (`id`, `username`, `password`, `token`, `active`, `id_role`, 
 (6, 'minhtest6', '$2a$07$ptmoOWLjwgZT6VlImAlYYeV7Fx789/SxtNChznXXcyhlAbiyjsbz2', 'b194f1cf64a8d8a32d1240b6fd261e53', 0, 1, '0000-00-00 00:00:00', '2016-09-13 19:59:55', '2016-09-13 17:59:55'),
 (7, 'minhtest7', '$2a$07$ptmqvBEBkpt0XhH6cVGQ3.2V78wmSdm/g9JrJlnnJGUfqWfa8VDkK', 'cb81cd80a6712216a75cc7460d13bb87', 0, 1, '0000-00-00 00:00:00', '2016-09-14 01:22:20', '2016-09-13 18:22:20'),
 (8, 'minhtest8', '$2a$07$ptmq5dHVG85giiRDGWzGRO0iQTurIcXLDxyVbmBjeyKeooVCdvL4a', '563f40ddafb7589633158a38b9c5b4b1', 0, 1, '0000-00-00 00:00:00', '2016-09-13 20:24:12', '2016-09-13 18:24:12'),
-(9, 'minhtest9', '$2a$07$ptm4VkYjX5Xfx5rICAfpjOWVvNyN5v/pjdggca2JHrBa/gBa.x/wu', '9be11a925645c25d4f1c4d6e6f9e1487', 0, 1, '0000-00-00 00:00:00', '2016-09-14 01:28:13', '2016-09-13 18:28:13'),
-(10, 'minhtest10', '$2a$07$ptmRZCosDZmP27YDqZ02W.rFRu6BQQDsavfx.SBWfJA7.06ORx4Iq', '85781e8006fa980178b1471028c055bf', 0, 1, '0000-00-00 00:00:00', '2016-09-14 01:30:28', '2016-09-13 18:30:28');
+(9, 'minhtest9', '$2a$07$ptm4VkYjX5Xfx5rICAfpjOWVvNyN5v/pjdggca2JHrBa/gBa.x/wu', '6fa89c9293da470e7ad3bbb2d9b3aacb', 0, 1, '2016-09-19 23:30:53', '2016-09-14 01:28:13', '2016-09-19 16:30:53'),
+(10, 'minhtest10', '$2a$07$ptmRZCosDZmP27YDqZ02W.rFRu6BQQDsavfx.SBWfJA7.06ORx4Iq', 'b0cac10c096f90b5c2d95a6ae7167f1f', 0, 3, '2016-09-19 21:06:01', '2016-09-14 01:30:28', '2016-09-19 14:06:01');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `user_report`
+--
+
+CREATE TABLE `user_report` (
+  `id` int(10) UNSIGNED NOT NULL,
+  `user_id` int(10) UNSIGNED NOT NULL,
+  `title` text NOT NULL,
+  `message` int(11) NOT NULL,
+  `reported_in` varchar(255) NOT NULL,
+  `created` datetime NOT NULL,
+  `updated` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
 -- Indexes for dumped tables
 --
+
+--
+-- Indexes for table `bill`
+--
+ALTER TABLE `bill`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `profile_id` (`profile_id`),
+  ADD KEY `user_id` (`user_id`),
+  ADD KEY `order_id` (`order_id`);
 
 --
 -- Indexes for table `category`
@@ -250,12 +293,25 @@ ALTER TABLE `role`
 --
 ALTER TABLE `user`
   ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `username` (`username`),
   ADD KEY `id_role` (`id_role`);
+
+--
+-- Indexes for table `user_report`
+--
+ALTER TABLE `user_report`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `user_id` (`user_id`);
 
 --
 -- AUTO_INCREMENT for dumped tables
 --
 
+--
+-- AUTO_INCREMENT for table `bill`
+--
+ALTER TABLE `bill`
+  MODIFY `id` int(11) UNSIGNED NOT NULL AUTO_INCREMENT;
 --
 -- AUTO_INCREMENT for table `category`
 --
@@ -297,8 +353,21 @@ ALTER TABLE `role`
 ALTER TABLE `user`
   MODIFY `id` int(11) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
 --
+-- AUTO_INCREMENT for table `user_report`
+--
+ALTER TABLE `user_report`
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT;
+--
 -- Constraints for dumped tables
 --
+
+--
+-- Constraints for table `bill`
+--
+ALTER TABLE `bill`
+  ADD CONSTRAINT `bill_ibfk_1` FOREIGN KEY (`profile_id`) REFERENCES `profile` (`id`),
+  ADD CONSTRAINT `bill_ibfk_2` FOREIGN KEY (`user_id`) REFERENCES `user` (`id`),
+  ADD CONSTRAINT `bill_ibfk_3` FOREIGN KEY (`order_id`) REFERENCES `order_product` (`id`);
 
 --
 -- Constraints for table `image`
@@ -331,6 +400,12 @@ ALTER TABLE `profile`
 --
 ALTER TABLE `user`
   ADD CONSTRAINT `user_ibfk_1` FOREIGN KEY (`id_role`) REFERENCES `role` (`id`);
+
+--
+-- Constraints for table `user_report`
+--
+ALTER TABLE `user_report`
+  ADD CONSTRAINT `user_report_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `user` (`id`);
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
