db535613508e785c6cc3effa0fb17adeb4659cf6
trungducng <nguyentrungduc2910@gmail.com>
2016-09-25 05:25:15 +0700

    Add files via upload
    productlist

--- backend-4c/Administration/Models/User.php
+++ backend-4c/Administration/Models/User.php
@@ -8,9 +8,10 @@
 
 namespace Administration\Models;
 
+use Library\Core\UserModel;
 use Library\Core\Model as MainModel;
 
-class User extends MainModel
+class User extends MainModel implements UserModel
 {
     protected $table = 'user';
     protected $primary = 'id';
@@ -20,28 +21,20 @@ class User extends MainModel
         parent::__construct($co);
     }
 
-    /**
-     * insertUser() this function will choose/define which data from $_POST method to be inserted to table User
-     * @param array $post
-     * @return bool
-     */
+
     public function insertUser($post)
     {
         return $this->insert(array(
             'username' => $post['username'],
             'password' => $this->blowfishHasher($post['password']),
             'token' => md5(uniqid() . time()), //create random token
-            'id_role' => $post['role'],
-            'created' => date("Y:m:d H:i:s")
+            'id_role' => isset($post['role']) ? $post['role'] : 4,
+            'created' => date("Y:m:d H:i:s"),
+            'active' => isset($post['active']) ? $post['active'] : 1,
         ));
     }
 
-    /**
-     * TODO getUserLogin
-     * @param string $name
-     * param string $password
-     * @return object result
-     */
+
     public function getUserLogin($login, $password)
     {
         $result = $this->fetchAll("username= '$login'");
@@ -54,15 +47,14 @@ class User extends MainModel
         return null;
     }
 
-    public function delete($id){
 
+    public function retrieveLoginByToken($token)
+    {
+        $result = $this->fetchAll("token= '$token'");
+        return isset($result[0]) ? $result[0] : null;
     }
-    /**
-     * function update token
-     * @param $token
-     * @param $id
-     * @return boolean
-     */
+
+
     public function updateToken($token, $id)
     {
         return $this->update(array(
@@ -71,16 +63,24 @@ class User extends MainModel
             ' id = ' . $id);
     }
 
-    /**
-     * update last login time
-     * @param $time
-     * @param $id
-     * @return boolean
-     */
+
     public function updateLastLogin($time, $id)
     {
         return $this->update(array(
             'last_login' => isset($time) ? $time : ''
         ), ' id = ' . $id);
     }
+
+    public function modifyUser($post, $id)
+    {
+        // TODO: Implement modifyUser() method.
+    }
+
+    public function getUserByUsername($username)
+    {
+        // TODO: Implement getUserByName() method.
+        return $this->fetchAll("username= '$username' ");
+    }
+
+
 }
