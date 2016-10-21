ab6609162f5bbdd9ef01c4026a5fceb5899104b4
Phan Minh <phanminh65@gmail.com>
2016-10-22 00:28:40 +0700

    fix fb comment

--- backend-4c/Application/Views/Controllers/Product/index.php
+++ backend-4c/Application/Views/Controllers/Product/index.php
@@ -1,17 +1,20 @@
 <body>
 <script>
-    window.fbAsyncInit = function() {
+    window.fbAsyncInit = function () {
         FB.init({
-            appId      : '340915906262846',
-            xfbml      : true,
-            version    : 'v2.8'
+            appId: '340915906262846',
+            xfbml: true,
+            version: 'v2.8'
         });
     };
 
-    (function(d, s, id){
+    (function (d, s, id) {
         var js, fjs = d.getElementsByTagName(s)[0];
-        if (d.getElementById(id)) {return;}
-        js = d.createElement(s); js.id = id;
+        if (d.getElementById(id)) {
+            return;
+        }
+        js = d.createElement(s);
+        js.id = id;
         js.src = "//connect.facebook.net/en_US/sdk.js";
         fjs.parentNode.insertBefore(js, fjs);
     }(document, 'script', 'facebook-jssdk'));
@@ -155,15 +158,20 @@
                                     fugiat, minima quaerat necessitatibus? Optio adipisci ab, obcaecati, porro unde
                                     accusantium facilis repudiandae.</p>
                             </div>
-                            <div class="col-md-12"><hr ></div>
-                            <div class="fb-comments" data-href="http://www.hanu4c13.html-5.me/" data-numposts="5"></div>
-                            <div
-                                class="fb-like"
-                                data-share="true"
-                                data-width="450"
-                                data-show-faces="true">
+                            <div class="col-md-12">
+                                <hr>
                             </div>
+
+
                             <div class="tab-pane fade " id="review">
+                                <div class="col-md-offset-3 col-md-9">
+                                    <!--                            FACEBOOK COMMENT-->
+                                    <div class="fb-comments"
+                                         data-href="http://<?= $_SERVER['HTTP_HOST'] . strtok($_SERVER["REQUEST_URI"], '?') ?>"
+                                         data-numposts="5"></div>
+                                    <div class="fb-like" data-share="true" data-width="999"
+                                         data-show-faces="true"></div>
+                                </div>
                                 <div class="aa-product-review-area">
                                     <h4>2 Reviews for T-Shirt</h4>
                                     <ul class="aa-review-nav">
