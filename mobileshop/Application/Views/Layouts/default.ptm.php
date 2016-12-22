7dfec25e97c92e31cc7b25bb8fdeb77151a83d29
Minh Phan <phanminh65@gmail.com>
2016-12-22 22:56:40 +0700

    change directory project to mobileshop

--- mobileshop/Application/Views/Layouts/default.ptm.php
+++ mobileshop/Application/Views/Layouts/default.ptm.php
@@ -178,7 +178,7 @@
                             <!-- Text based logo -->
                             <a href="index.html">
                                 <span class="fa fa-mobile"></span>
-                                <p>SV<strong>Mobile</strong> <span>Your Shopping Partner</span></p>
+                                <p>AT<strong>Mobile</strong> <span>Your Shopping Partner</span></p>
                             </a>
                             <!-- img based logo -->
                             <!-- <a href="index.html"><img src="img/logo.jpg" alt="logo img"></a> -->
